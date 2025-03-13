f96e8f98533c3662cca9ef656fcec96be612310c
kelvin okumu <105194367+kelvinokumu@users.noreply.github.com>
2025-03-13 11:16:24 +0300

    new sort

---
+++ src/dsa/sorting/InsertionSort.java
@@ -0,0 +1,7 @@
+package dsa.sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        System.out.println("Insertion Sort");
+    }
+}
--- src/dsa/test_questions/Question1.java
+++ src/dsa/test_questions/Question1.java
@@ -3,19 +3,25 @@ package dsa.test_questions;
 import java.util.Arrays;
 import java.util.List;
 
+//import java.util.*;
+
 public class Question1 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(3, 7, 2, 9, 5);
+
         int max = findLargest(numbers);
         System.out.println("Largest Number: " + max);
+
+//      System.out.println("Largest Number: " + findLargest(numbers));
     }
 
     public static int findLargest(List<Integer> numbers) {
         int max = numbers.get(0); //  first number is the largest
 
-        for (int i = 1; i < numbers.size(); i++) {
+        for (int i = 1; i < numbers.size(); i++) //loop through all values
+        {
             if (numbers.get(i) > max) {
-                max = numbers.get(i);
+                max = numbers.get(i); // swap if the value is bigger than the current
             }
         }
         return max;
